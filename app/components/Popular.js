1cbd39591f3b4a332e78f572b1f5fb7f8b1b94b3
Josélia L. Costa <joselialcosta@gmail.com>
2020-02-29 15:59:37 -0300

    Adds state to click and modify color of language according with selected language

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React from "react";
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: "All"
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    });
+  }
+
   render() {
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
 
     return (
-      <ul className='flex-center'>
-        {languages.map((language) => (
-          <li key={language}>
-            <button className='btn-clear nav-link'>{language}</button>
+      <ul className="flex-center">
+        {languages.map(language => (
+          <li
+            key={language}
+            style={
+              language === this.state.selectedLanguage ? { color: "red" } : null
+            }
+          >
+            <button
+              className="btn-clear nav-link"
+              onClick={() => this.updateLanguage(language)}
+            >
+              {language}
+            </button>
           </li>
         ))}
       </ul>
-    )
+    );
   }
 }
