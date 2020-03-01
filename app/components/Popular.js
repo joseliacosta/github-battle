055cfd4942f62f3221681f2ec5d4daff73dcd7e9
Josélia L. Costa <joselialcosta@gmail.com>
2020-03-01 20:58:58 -0300

    Conects with github API to fetching repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { fetchPopularRepos } from "../utils/api";
 
 const LanguagesNav = ({ selected, onUpdateLanguage }) => {
   const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
@@ -31,26 +32,57 @@ export default class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: "All"
+      selectedLanguage: "All",
+      repos: null,
+      error: null
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
   }
 
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
   updateLanguage(selectedLanguage) {
     this.setState({
-      selectedLanguage
+      selectedLanguage,
+      repos: null,
+      error: null
     });
+
+    fetchPopularRepos(selectedLanguage)
+      .then(repos =>
+        this.setState({
+          repos,
+          error: null
+        })
+      )
+      .catch(() => {
+        console.warn("Error fetching repos: ", error);
+        this.setState({
+          error: `There was an error fetching the repositories.`
+        });
+      });
+  }
+
+  isLoading() {
+    return this.state.repos === null && this.state.error === null;
   }
 
   render() {
-    const { selectedLanguage } = this.state;
+    const { selectedLanguage, repos, error } = this.state;
 
     return (
-      <LanguagesNav
-        selected={selectedLanguage}
-        onUpdateLanguage={this.updateLanguage}
-      />
+      <>
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+        {this.isLoading() && <p>LOADING</p>}
+        {error && <p>{error}</p>}
+        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+      </>
     );
   }
 }
