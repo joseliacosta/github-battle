5199cd178056fa443dcf0cb12d0d09b435409393
Josélia L. Costa <joselialcosta@gmail.com>
2020-03-01 19:45:25 -0300

    Adds prop-types to Nav component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const LanguagesNav = ({ selected, onUpdateLanguage }) => {
   const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
@@ -20,6 +21,11 @@ const LanguagesNav = ({ selected, onUpdateLanguage }) => {
       ))}
     </ul>
   );
+};
+
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
 };
 export default class Popular extends React.Component {
   constructor(props) {
