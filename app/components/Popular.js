94db8bf9e041380409388ba1a37ed77a212f42e8
Joselia Costa <joselialcosta@gmail.com>
2021-06-06 17:22:25 -0300

    refactors .repos style to make it reusable

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -38,7 +38,7 @@ function ReposGrid ({ repos }) {
         const { login, avatar_url } = owner
 
         return (
-          <li key={html_url} className='repo bg-light'>
+          <li key={html_url} className='card bg-light'>
             <h4 className='header-lg center-text'>
               #{index + 1}
             </h4>
@@ -137,7 +137,7 @@ export default class Popular extends React.Component {
           onUpdateLanguage={this.updateLanguage}
         />
         {this.isLoading() && <p>LOADING</p>}
-        {error && <p>{error}</p>}
+        {error && <p className="center-text error">{error}</p>}
         {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]}/>}
       </>
     );
